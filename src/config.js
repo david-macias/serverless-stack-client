211a2553eb3df865df5db8c7e3e57c77742d1bee
David Macias <dmacias@gmail.com>
2020-01-14 21:00:37 -0500

    Setting up React app.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-9779"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://6m0t44bnja.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_duxLLUC8F",
+      APP_CLIENT_ID: "57ehn2gehl04phcspbhg93hu4f",
+      IDENTITY_POOL_ID: "us-east-1:f9706d0b-9e50-48fb-80e7-064ea23ddf8a"
+    }
+  };
